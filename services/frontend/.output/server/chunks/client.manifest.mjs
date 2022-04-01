b00eab1648cdefb7c677347e312a5a5c9b014c52
MateBook <examletru@gmail.com>
2022-04-01 17:25:47 +0300

    first commit

---
+++ services/frontend/.output/server/chunks/client.manifest.mjs
@@ -0,0 +1,12 @@
+const client_manifest = {
+  "node_modules/nuxt3/dist/app/entry.mjs": {
+    "file": "entry-e4c67f1d.mjs",
+    "src": "node_modules/nuxt3/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.d1fbfbe7.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
